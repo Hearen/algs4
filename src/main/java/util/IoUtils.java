32ac53b9acea8533c89ab2e28972bed398d5e12b
LHearen <lhearen@gmail.com>
2018-07-18 08:48:19 +0800

    add resources

--- src/main/java/util/IoUtils.java
+++ src/main/java/util/IoUtils.java
@@ -1,4 +1,12 @@
 package util;
 
+import java.nio.file.Paths;
+
 public class IoUtils {
+
+    public static String getResourcePath(String fileName) {
+        String resAbsolutePath = Paths.get("").toAbsolutePath().toString();
+        String filePath = resAbsolutePath.concat("/src/main/resources/").concat(fileName);
+        return filePath;
+    }
 }
